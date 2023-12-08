4e0c45600327cc99b307ee8309ded9b573018199
Biraj Shrestha <86864374+Biraj1234@users.noreply.github.com>
2023-12-08 14:33:09 +0545

    first commit

---
+++ database/migrations/2023_12_07_085217_create_staffs_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('staffs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('designation_id')->nullable();
+            $table->text('department_id')->nullable();
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('staffs');
+    }
+};
---
+++ resources/views/scrapper.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="s-segment">
+        <!-- this commented segment is for SECRETARY SECRETARIAT -->
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="s-title-section">Industry</div>
+                <div class="single-profile iins s-first">
+                    <div class="profile-img">
+                        <img src="https://moics.gov.np/uploads/shares/mukunda1_0.jpg" alt="Mukunda Prasad Niroula"
+                            title="Mukunda Prasad Niroula">
+                    </div>
+                    <div class="profile-content">
+                        <p class="name">Mukunda Prasad Niroula</p>
+                        <p class="position">Secretary</p>
+                        <a target="_blank" href="https://moics.gov.np/staffdetail/273" class="view-detail">View More</a>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="col-lg-6">
+                <div class="s-title-section">Commerce and Supplies</div>
+
+            </div>
+
+        </div>
+
+
+
+    </div>
+
+</body>
+
+</html>
